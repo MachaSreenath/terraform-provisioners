058955ece551425019bb21787dc270a53d45bb26
MachaSreenath <sreenathshree9@gmail.com>
2024-12-27 16:55:07 +0530

    terraform-provisioners changes

--- ec2.tf
+++ ec2.tf
@@ -7,6 +7,20 @@ resource "aws_instance" "web" {
   }
 
   provisioner "local-exec" {
+    command = "echo this will execute at the time of creation, you can trigger other system like email and sending alerts" # self = aws_instance.web
+  }
+
+  provisioner "local-exec" {
     command = "echo ${self.private_ip} > inventory" # self = aws_instance.web
+  }
+
+  
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory web.yaml" # self = aws_instance.web
+  }
+
+  provisioner "local-exec" {
+    command = "echo this will execute at the time of destroy, you can trigger other system like email and sending alerts" # self = aws_instance.web
+    when = destroy
   }
 }
